6006c67e25274d4d7b1f06e58422e58a92d7df31
ftahira1 <123195285+ftahira1@users.noreply.github.com>
2023-06-09 12:13:22 -0400

    Added prompts

--- db/schema.sql
+++ db/schema.sql
@@ -13,13 +13,13 @@ CREATE TABLE roles (
     id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     department_id INT NOT NULL,
-    salary DECIMAL,
+    salary DECIMAL(10, 2),
     PRIMARY KEY (id),
     FOREIGN KEY (department_id)
         REFERENCES departments(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
